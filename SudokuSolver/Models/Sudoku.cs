a475667b83b996a0556898ead8af074ed65d6d65
Samir <photography.sam@gmail.com>
2017-05-12 20:42:25 +0800

    extended to go with >2 branches

--- SudokuSolver/Models/Sudoku.cs
+++ SudokuSolver/Models/Sudoku.cs
@@ -14,9 +14,9 @@ namespace SudokuSolver.Models
         {
             get
             {
-                foreach(var b in Blocks)
+                foreach (var b in Blocks)
                 {
-                    foreach(var c in b.Cells)
+                    foreach (var c in b.Cells)
                     {
                         if (c.Value == 0)
                             return false;
@@ -54,12 +54,12 @@ namespace SudokuSolver.Models
             List<Column> columns = BuildSudokuColumns();
             List<Row> rows = BuildSudokuRows();
 
-            for(int i=1;i<10;i++)
+            for (int i = 1; i < 10; i++)
             {
                 Block b = new Block();
                 b.Id = i;
                 b.Cells = new List<Cell>();
-                for(int j=1;j<10;j++)
+                for (int j = 1; j < 10; j++)
                 {
                     int index = (i * 9) - (9 - j);
                     Cell c = new Cell();
@@ -127,31 +127,31 @@ namespace SudokuSolver.Models
 
             Row one = new Row();
             one.Id = 1;
-            one.CellNumbers = new List<int> {1,2,3,10,11,12,19,20,21 };
+            one.CellNumbers = new List<int> { 1, 2, 3, 10, 11, 12, 19, 20, 21 };
             Row two = new Row();
             two.Id = 2;
             two.CellNumbers = new List<int> { 4, 5, 6, 13, 14, 15, 22, 23, 24 };
             Row three = new Row();
             three.Id = 3;
-            three.CellNumbers = new List<int> { 7,8,9,16,17,18,25,26,27 };
+            three.CellNumbers = new List<int> { 7, 8, 9, 16, 17, 18, 25, 26, 27 };
             Row four = new Row();
             four.Id = 4;
-            four.CellNumbers = new List<int> { 28,29,30,37,38,39,46,47,48 };
+            four.CellNumbers = new List<int> { 28, 29, 30, 37, 38, 39, 46, 47, 48 };
             Row five = new Row();
             five.Id = 5;
-            five.CellNumbers = new List<int> {31,32,33,40,41,42,49,50,51 };
+            five.CellNumbers = new List<int> { 31, 32, 33, 40, 41, 42, 49, 50, 51 };
             Row six = new Row();
             six.Id = 6;
-            six.CellNumbers = new List<int> { 34,35,36,43,44,45,52,53,54 };
+            six.CellNumbers = new List<int> { 34, 35, 36, 43, 44, 45, 52, 53, 54 };
             Row seven = new Row();
             seven.Id = 7;
-            seven.CellNumbers = new List<int> { 55,56,57,64,65,66,73,74,75 };
+            seven.CellNumbers = new List<int> { 55, 56, 57, 64, 65, 66, 73, 74, 75 };
             Row eight = new Row();
             eight.Id = 8;
-            eight.CellNumbers = new List<int> { 58,59,60,67,68,69,76,77,78 };
+            eight.CellNumbers = new List<int> { 58, 59, 60, 67, 68, 69, 76, 77, 78 };
             Row nine = new Row();
             nine.Id = 9;
-            nine.CellNumbers = new List<int> { 61,62,63,70,71,72,79,80,81 };
+            nine.CellNumbers = new List<int> { 61, 62, 63, 70, 71, 72, 79, 80, 81 };
 
             result.Add(one);
             result.Add(two);
@@ -200,9 +200,9 @@ namespace SudokuSolver.Models
                         {
                             Debugging(child);
                             var c = Solution;
-                            child = TryLuckWithChild(child); // exception if wrong path found
+                            child = TryLuckWithChild(child, 2); // exception if wrong path found
                         }
-                        catch(Exception)
+                        catch (Exception)
                         {
                             Children.Remove(child);
                             break;
@@ -211,7 +211,7 @@ namespace SudokuSolver.Models
                     OnRaiseSudokuUpdatedEvent(new SudokuUpdatedEventArgs(child));
                 } while (!child.IsSolved);
 
-                if(child.IsSolved)
+                if (child.IsSolved)
                 {
                     Children.Remove(child);
                     OnRaiseSudokuSolvedEvent(new SudokuUpdatedEventArgs(child));
@@ -267,25 +267,26 @@ namespace SudokuSolver.Models
 
         }
 
-        private Sudoku TryLuckWithChild(Sudoku s)
+        private Sudoku TryLuckWithChild(Sudoku s, int branches)
         {
             Debug.WriteLine("I am being called ...");
             bool foundAnchor = false;
             int idOfAnchorCell = 0;
-            int firstValue = 0; ;
-            int secondValue = 0;
+            var values = new int[branches];
             foreach (var b in s.Blocks)
             {
                 foreach (var c in b.Cells)
                 {
-                    if (c.PossibleValues.Count == 2 && c.Value == 0)
+                    if (c.PossibleValues.Count == branches && c.Value == 0)
                     {
                         Debug.WriteLine("Cell ID " + c.Id + " has 2 possible values");
                         foundAnchor = true;
-                        firstValue = c.PossibleValues.ElementAt(0);
-                        secondValue = c.PossibleValues.ElementAt(1);
+                        for (int i = 0; i < branches; i++)
+                        {
+                            values[i] = c.PossibleValues.ElementAt(i);
+                        }
                         idOfAnchorCell = c.Id;
-                        c.Value = firstValue;
+                        c.Value = values[0];
                         break;
                     }
                     if (c.PossibleValues.Count == 0 && c.Value == 0)
@@ -297,23 +298,25 @@ namespace SudokuSolver.Models
 
             if (foundAnchor)
             {
-
-                Sudoku child = (Sudoku)s.Clone();
-                foreach (var b in child.Blocks)
+                for (int i = 1; i < values.Length; i++)
                 {
-                    foreach (var c in b.Cells)
+                    Sudoku child = (Sudoku)s.Clone();
+                    foreach (var b in child.Blocks)
                     {
-                        if (c.Id == idOfAnchorCell)
+                        foreach (var c in b.Cells)
                         {
-                            c.Value = secondValue;
+                            if (c.Id == idOfAnchorCell)
+                            {
+                                c.Value = values[i];
+                            }
                         }
                     }
+                    Children.Add(child);
                 }
-                Children.Add(child);
             }
             else
             {
-                throw new Exception(); //too hard to solve, no unique solution
+                return TryLuckWithChild(s, branches + 1);
             }
 
             return s;
@@ -321,7 +324,7 @@ namespace SudokuSolver.Models
 
         private void Debugging(Sudoku s)
         {
-            foreach(var b in s.Blocks)
+            foreach (var b in s.Blocks)
             {
                 foreach (var c in b.Cells)
                 {
@@ -333,13 +336,13 @@ namespace SudokuSolver.Models
         public object Clone()
         {
             Sudoku s = new Sudoku();
-            foreach(var b in this.Blocks)
+            foreach (var b in this.Blocks)
             {
-                foreach(var c in b.Cells)
+                foreach (var c in b.Cells)
                 {
-                    foreach(var bb in s.Blocks)
+                    foreach (var bb in s.Blocks)
                     {
-                        foreach(var cc in bb.Cells)
+                        foreach (var cc in bb.Cells)
                         {
                             if (c.Id == cc.Id)
                                 cc.Value = c.Value;
